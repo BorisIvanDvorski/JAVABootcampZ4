195584b7b67562a4e3c4d444a93b49a6486351d6
Boris Ivan Dvorski <boris.dvorski@gmail.com>
2023-03-20 09:30:15 +0100

    Implemented user input into HashMap, print out HashMap according to the inputs.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your favorite books and authors. Enter 'stop' to finish.");
+        Map<String,String> map = new HashMap<>();
+        while(true){
+            System.out.print("Book title: ");
+            String key = input.next();
+            if(key.equals("stop")){
+               break;
+            }
+            else{
+                System.out.print("Author name: ");
+                String value = input.next();
+                map.put(key,value);
+            }
+        }
+        for(String i : map.keySet()) {
+            System.out.println(i+" by "+map.get(i));
+        }
     }
 }
